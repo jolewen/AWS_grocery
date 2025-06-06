363766c3774738dc4b8643691291c3216350f5eb
johannes <johannes.lewen@lewen.eu>
2025-06-06 21:42:11 +0200

    incorporate db setup into deployment

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -18,28 +18,9 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "tls_private_key" "ec2_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "generated_key" {
-  key_name   = "tf-generated-key"
-  public_key = tls_private_key.ec2_key.public_key_openssh
-}
-
-resource "aws_secretsmanager_secret" "ec2_key_secret" {
-  name = "ec2-key-private"
-}
-
-resource "aws_secretsmanager_secret_version" "ec2_key_secret_version" {
-  secret_id     = aws_secretsmanager_secret.ec2_key_secret.id
-  secret_string = tls_private_key.ec2_key.private_key_pem
-}
-
 resource "aws_instance" "webserver" {
   instance_type = "t2.micro"
-  key_name = aws_key_pair.generated_key.key_name
+  key_name = "webserver-private-pair"
   launch_template { 
   id = "lt-0568aae9bc6372ad5"
   version = "$Latest"
@@ -52,7 +33,7 @@ resource "aws_db_instance" "postgres" {
   engine_version          = "15.4"
   instance_class          = var.db_instance_class
   allocated_storage       = var.db_allocated_storage
-  db_name                    = var.db_name
+  db_name                 = var.db_name
   username                = var.db_username
   password                = var.db_password
   publicly_accessible     = false
