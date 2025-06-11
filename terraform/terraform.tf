5cb40136d448d298af150942b6ba416eb398251a
johannes <johannes.lewen@lewen.eu>
2025-06-11 16:38:42 +0200

    fix iam role attachment to ec2

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -74,7 +74,7 @@ resource "aws_instance" "webserver" {
   instance_type = "t2.micro"
   key_name = "webserver-private-pair"
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-  security_groups = [aws_security_group.webstore_sg.id]
+  vpc_security_group_ids = [aws_security_group.webstore_sg.id]
   launch_template {
     id = "lt-0c7204c7a02b17cbe"
     version = "$Latest"
