2a821a2a4ee16ed64ce30a25e2b1ec25b36ce1e4
johannes <johannes.lewen@lewen.eu>
2025-06-11 14:31:05 +0200

    fix iam role attachment to ec2

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -83,11 +83,11 @@ resource "aws_instance" "webserver" {
 
 resource "aws_db_instance" "postgres" {
   identifier              = var.db_identifier
-  # engine                  = "postgres"
+  engine                  = "postgres"
   engine_version          = "15.13"
   instance_class          = var.db_instance_class
   allocated_storage       = var.db_allocated_storage
-  db_name                 = var.db_name
+  # db_name                 = var.db_name
   username                = var.db_username
   password                = var.db_password
   publicly_accessible     = false
